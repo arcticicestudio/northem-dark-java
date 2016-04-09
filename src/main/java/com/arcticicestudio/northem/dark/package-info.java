76c0bd70d175a233d6f9138a58a60e5ac8f52b3e
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 13:58:36 +0200

    Create package info for "com.arcticicestudio.northem.dark"

---
+++ src/main/java/com/arcticicestudio/northem/dark/package-info.java
@@ -0,0 +1,49 @@
+/*
+++++++++++++++++++++++++++++++++++++++++++++++
+title     Package Info                       +
+project   northem-dark-java                  +
+file      package-info.java                  +
+version                                      +
+author    Arctic Ice Studio                  +
+email     development@arcticicestudio.com    +
+website   http://arcticicestudio.com         +
+copyright Copyright (C) 2016                 +
+created   2016-01-23 15:44 UTC+0100          +
+modified  2016-04-09 13:50 UTC+0200          +
+++++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+Root package of the "Northem Dark - Java" project, which implements the "Northem Dark" color palette as enumeration.
+The package represents the public API.
+The main entry point of the package is the "NorthemDark" class.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+Northem Dark
+  (https://github.com/arcticicestudio/northem-dark)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JavaFX 2.2 API Documentation
+  (https://docs.oracle.com/javafx/2/api/)
+JavaFX 2.2 Oracle Project Website
+  (http://docs.oracle.com/javase/8/javase-clienttechnologies.htm)
+Arctic Versioning Specification
+  (http://specs.arcticicestudio.com/arcver) (ArcVer)
+*/
+/**
+ * Root package of the <a href="https://github.com/arcticicestudio/northem-dark-java">Northem Dark - Java</a>
+ * project, which implements the <a href="https://github.com/arcticicestudio/northem-dark">Northem Dark</a>
+ * color palette as enumeration.
+ * <p>
+ *   The package represents the public API.
+ *   The main entry point of the package is the {@link com.arcticicestudio.northem.dark.NorthemDark} class.
+ * </p>
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @see <a href="https://github.com/arcticicestudio/northem-dark-java">Northem Dark - Java</a>
+ * @see <a href="https://github.com/arcticicestudio/northem-dark">Northem Dark</a>
+ * @since 0.1.0
+ */
+package com.arcticicestudio.northem.dark;
