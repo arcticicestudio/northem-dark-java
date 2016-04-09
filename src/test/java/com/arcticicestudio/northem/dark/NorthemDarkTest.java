620f137fa8330b711bac97c9faa70fbc5a626609
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 14:07:08 +0200

    Create JUnit test class for "com.arcticicestudio.northem.NorthemDark"

---
+++ src/test/java/com/arcticicestudio/northem/dark/NorthemDarkTest.java
@@ -0,0 +1,49 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++++++
+title     Northem Dark Enumeration JUnit Test +
+project   northem-dark-java                   +
+file      NorthemDarkTest.java                +
+version                                       +
+author    Arctic Ice Studio                   +
+email     development@arcticicestudio.com     +
+website   http://arcticicestudio.com          +
+copyright Copyright (C) 2016                  +
+created   2016-01-23 15:47 UTC+0100           +
+modified  2016-04-09 13:52 UTC+0200           +
++++++++++++++++++++++++++++++++++++++++++++++++
+
+[Description]
+JUnit test class for the 'NorthemDark' class of the "Northem Dark - Java" API.
+
+[Copyright]
+Copyright (C) 2016 Arctic Ice Studio <development@arcticicestudio.com>
+
+[References]
+Northem Dark
+  (https://github.com/arcticicestudio/northem-dark)
+Java 8 API Documentation
+  (https://docs.oracle.com/javase/8/docs/api/)
+JavaFX 2.2 API Documentation
+  (https://docs.oracle.com/javafx/2/api/)
+JavaFX 2.2 Oracle Project Website
+  (http://docs.oracle.com/javase/8/javase-clienttechnologies.htm)
+Arctic Versioning Specification
+  (http://specs.arcticicestudio.com/arcver) (ArcVer)
+*/
+
+package com.arcticicestudio.northem.dark;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * JUnit test class for the {@link NorthemDark} class of the
+ * <a href="https://github.com/arcticicestudio/northem-dark-java">Northem Dark - Java</a> API.
+ *
+ * @author Arctic Ice Studio &lt;development@arcticicestudio.com&gt;
+ * @see NorthemDark
+ * @see <a href="http://junit.org/">http://junit.org/</a>
+ * @see <a href="https://github.com/arcticicestudio/northem-dark-java">Northem Dark - Java</a>
+ * @since 0.1.0
+ */
+public class NorthemDarkTest {}
