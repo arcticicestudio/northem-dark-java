f33f5aed8020c4735018dd08dbf240d4c7cf9e12
Arctic Ice Studio <development@arcticicestudio.com>
2016-04-09 14:08:28 +0200

    Implement test method "shouldReturnTheCorrectRGBColorCode()"

--- src/test/java/com/arcticicestudio/northem/dark/NorthemDarkTest.java
+++ src/test/java/com/arcticicestudio/northem/dark/NorthemDarkTest.java
@@ -9,7 +9,7 @@ email     development@arcticicestudio.com     +
 website   http://arcticicestudio.com          +
 copyright Copyright (C) 2016                  +
 created   2016-01-23 15:47 UTC+0100           +
-modified  2016-04-09 13:52 UTC+0200           +
+modified  2016-04-09 14:08 UTC+0200           +
 +++++++++++++++++++++++++++++++++++++++++++++++
 
 [Description]
@@ -64,5 +64,24 @@ public class NorthemDarkTest {
     assertEquals(NorthemDark.hex(NorthemDark.ORANGE.get()), "#F0A06F");
     assertEquals(NorthemDark.hex(NorthemDark.LIME.get()), "#C9CC78");
     assertEquals(NorthemDark.hex(NorthemDark.PURPLE.get()), "#C2A1CA");
+  }
+
+  @Test
+  public void shouldReturnTheCorrectRGBColorCode() {
+    assertEquals(NorthemDark.rgb(NorthemDark.DARKEST_COAL.get()), "rgb(31, 31, 35)");
+    assertEquals(NorthemDark.rgb(NorthemDark.DARK_COAL.get()), "rgb(36, 36, 41)");
+    assertEquals(NorthemDark.rgb(NorthemDark.COAL.get()), "rgb(41, 41, 46)");
+    assertEquals(NorthemDark.rgb(NorthemDark.LIGHT_COAL.get()), "rgb(79, 79, 89)");
+    assertEquals(NorthemDark.rgb(NorthemDark.DIRTY_WHITE.get()), "rgb(222, 222, 222)");
+    assertEquals(NorthemDark.rgb(NorthemDark.WHITE.get()), "rgb(236, 238, 239)");
+    assertEquals(NorthemDark.rgb(NorthemDark.AQUA.get()), "rgb(143, 206, 200)");
+    assertEquals(NorthemDark.rgb(NorthemDark.LIGHT_BLUE.get()), "rgb(133, 193, 211)");
+    assertEquals(NorthemDark.rgb(NorthemDark.BLUE.get()), "rgb(135, 178, 206)");
+    assertEquals(NorthemDark.rgb(NorthemDark.DARK_BLUE.get()), "rgb(91, 129, 175)");
+    assertEquals(NorthemDark.rgb(NorthemDark.RED.get()), "rgb(224, 116, 115)");
+    assertEquals(NorthemDark.rgb(NorthemDark.YELLOW.get()), "rgb(253, 206, 132)");
+    assertEquals(NorthemDark.rgb(NorthemDark.ORANGE.get()), "rgb(240, 160, 111)");
+    assertEquals(NorthemDark.rgb(NorthemDark.LIME.get()), "rgb(201, 204, 120)");
+    assertEquals(NorthemDark.rgb(NorthemDark.PURPLE.get()), "rgb(194, 161, 202)");
   }
 }
